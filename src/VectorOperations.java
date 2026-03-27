b635a5b091ba6c8ea930a402bae0afa5a41874b8
Gauricode <gaurijkjay@gmail.com>
2026-03-27 09:42:49 +0530

    push

--- src/VectorOperations.java
+++ src/VectorOperations.java
@@ -8,7 +8,7 @@ public class VectorOperations {
         
         while (scanner.hasNextInt()) {
             int choice = scanner.nextInt();
-            
+           
             if (choice == 5) {
                 break;
             }
@@ -17,25 +17,44 @@ public class VectorOperations {
                 case 1:
                     // TODO: Read the name and add it to the vector
                     // Print "Added"
-                    
+
+                    String nameToAdd = scanner.next();
+                    students.add(nameToAdd);
+                    System.out.println("Added");
                     break;
                     
                 case 2:
                     // TODO: Read the name and the 1-based index
                     // Insert the name at the correct 0-based index in the vector
                     // Print "Inserted"
-                    
+		
+		     String nameToInsert = scanner.next();
+                    int index = scanner.nextInt(); // 1-based index
+
+                    if (index >= 1 && index <= students.size() + 1) {
+                        students.add(index - 1, nameToInsert); 
+                        System.out.println("Inserted");
+                    }
+
+
+
                     break;
                     
                 case 3:
                     // TODO: Read the name and remove it from the vector
                     // Print "Removed"
                     
+		    String nameToRemove = scanner.next();
+                    if (students.remove(nameToRemove)) {
+                        System.out.println("Removed");
+                    }
                     break;
                     
                 case 4:
                     // TODO: Display the vector
-                    
+
+	            
+                    System.out.println(students);
                     break;
                     
                 default:
